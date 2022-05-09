89799ac271d06b4679485c2fe417ebd2071d672f
TazarChriss <77143206+tazarchriss@users.noreply.github.com>
2022-05-09 23:48:14 +0300

    Create organizations.php

---
+++ organizations.php
@@ -0,0 +1,195 @@
+<!-- This is the page for managing all the staffs  --><!-- This is a page for Admin Home page -->
+<?php
+
+    session_start();
+    $id=$_SESSION['id'];
+    if ($_SESSION['role']!='1') {
+      header('Location:index.html');
+    }
+
+    else{
+      include 'config/connection.php';
+      $sql="SELECT * FROM host ";
+      $qry=mysqli_query($conn,$sql);
+      
+
+      include 'include/header.php';
+    ?>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+ 
+  
+<?php include 'include/anav.php'; ?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+            <!-- Manage staffs buttons -->
+          <div class="col-sm-6">
+            <a href="AddOrganization.php" class="btn bg-orange text-white "><i class=" fa fa-plus"></i> Add Organization</a>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item "><a href="admin.html" class="text-orange">Home</a></li>
+              <li class="breadcrumb-item ">Manage Organizations</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Start of Staff Table -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-md-12">
+        <div class="card">
+            <div class="card-header bg-dark">
+              <h3 class="card-title"></h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                     <?php
+
+                    if (mysqli_num_rows($qry) == 0){
+                   
+
+                    ?>
+                   <p class="text-info">No student was selected !</p>
+
+                    <?php
+
+                    }
+
+                    else{
+                      
+                      ?>
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>SN</th>
+                  <th>Organization Name</th>
+                  <th>Location</th>
+                  <th>Type</th>
+                  <th>Status</th>
+                  <th></th>
+           
+                </tr>
+                </thead>
+                <?php
+                    for ($i=1; $i<=mysqli_num_rows($qry); $i++){
+                      $row = mysqli_fetch_array($qry);
+                    
+                    //  $sql1="SELECT SUM(amount) FROM earning WHERE `userId`='$id' ";
+                    //  $qry1=mysqli_query($conn,$sql1);
+                    //  $res=mysqli_fetch_array($qry1);
+
+                  
+                  ?>
+            <tr>
+              <td>
+                <?php echo $i; ?></td>
+              <td>
+                <?php echo $row['name']; ?>
+              </td>
+              <td>
+              <?php echo $row['region']; ?>
+              </td>
+              <td>
+              <?php echo $row['type']; ?> 
+             </td>
+              <td>
+              </td>
+              <td>
+                  <div class="text-center">
+                      <a class="bg-dark p-1" href="EditEarning.php?id=<?php echo $row['id'];?>"><i class="fa fa-eye "></i></a>
+                      <a class="bg-orange p-1" href="config/DeleteEarning.php?id=<?php echo $row['id'];?>"><i class="fa fa-pen "></i></a>
+                  </div>
+              </td>
+            </tr>
+            <?php 
+              }
+            }
+              ?>
+              </tbody>
+              </table>
+            </div>
+            <!-- /.card-body -->
+          </div>
+        </div>
+          </div>
+          </div>
+    </section>
+   
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer bg-dark text-center">
+    <br>
+    <strong>Copyright &copy; 2021/2022 <a href="" class="text-orange">TazarChriss </a>.</strong>
+    All rights reserved.
+    <div class="mt-4">
+        <h1></h1>
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables  & Plugins -->
+<script src="plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="plugins/jszip/jszip.min.js"></script>
+<script src="plugins/pdfmake/pdfmake.min.js"></script>
+<script src="plugins/pdfmake/vfs_fonts.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+<!-- Page specific script -->
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["pdf"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
+</body>
+</html>
+
+
+<?php
+
+}
+?>
