994394f459723a8bf4c2846f574907ea126339d5
TazarChriss <77143206+tazarchriss@users.noreply.github.com>
2022-05-10 23:24:47 +0300

    Create hnav.php

---
+++ include/hnav.php
@@ -0,0 +1,98 @@
+<!-- This file contain the navbar of the host dashboard -->
+  
+   <!-- Navbar -->
+   <nav class="main-header navbar navbar-expand navbar-white bg-dark">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+        <a class="nav-link text-light" data-widget="pushmenu" href="#" role="button"><i class="fa fa-bars"></i></a>
+      </li>
+
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index.html" class="nav-link text-light text-bold">
+            <img src="logo.jpg" style="width:30px;">
+            MU-SOFAS
+        </a>
+      </li>
+  
+    </ul>
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+
+  
+      
+     
+        <li class="nav-item">
+          <a class="nav-link text-light" data-widget="fullscreen" href="#" role="button">
+        
+            <i class="fas fa-user"></i>  <?php echo $_SESSION['userID']; ?>
+          </a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link text-light" href="config/logout.php" role="button">
+            <i class="fas fa-power-off"></i> Log Out
+          </a>
+        </li>
+      </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-orange elevation-4">
+
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-1 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Username</a>
+        </div>
+      </div>
+
+ 
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          
+          <li class="nav-item menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="./index.html" class="nav-link active">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Dashboard v1</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./index2.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Dashboard v2</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./index3.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Dashboard v3</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+
+
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
