e93bf133125a1a7e9694ec270d2170babd653914
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-05-18 23:25:15 +0300

    Create confirmArrival.php

---
+++ config/confirmArrival.php
@@ -0,0 +1,30 @@
+<?php
+
+include 'connection.php';
+
+$id=$_GET['id'];
+$year = date("Y");
+$Date = "2019-05-10";
+
+// Add days to date and display it
+$end_date=date('Y-m-d', strtotime($Date. ' + 56 days'));
+
+$sql="UPDATE `allocation`  SET `alloc_status`='Arrived'  WHERE `alloc_id`='$id'";
+$qry=mysqli_query($conn,$sql);
+
+
+
+if (!$qry) {
+  die(mysqli_error($conn));
+}
+else
+{
+// $sql="INSERT INTO `allocation`(`attachment`,`year`,`start_date`,`end_date`) VALUES ('$id','$year','$Date','$end_date')";
+// $qry=mysqli_query($conn,$sql);
+// if (!$qry) {
+//   die(mysqli_error($conn));
+// }
+}
+
+
+?>
