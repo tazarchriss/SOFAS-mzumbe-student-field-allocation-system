3b14716a9e33edcacd37e0c11aaf86575ac5c2e3
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-05-23 23:54:02 +0300

    Update coordinatorNav.php

--- include/coordinatorNav.php
+++ include/coordinatorNav.php
@@ -13,17 +13,17 @@
             MU-SOFAS
         </a>
       </li>
-  
+
     </ul>
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
 
-  
-      
-     
+
+
+
         <li class="nav-item">
           <a class="nav-link text-light" >
-            <i class="fas fa-user"></i>  <?php echo $_SESSION['userID']; ?>
+            <i class="fas fa-user-tie"></i>  <?php echo $_SESSION['userID']; ?>
           </a>
         </li>
         <li class="nav-item">
@@ -44,29 +44,29 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-1 d-flex">
         <div class="image">
-          <img src="user.png" class="img-circle elevation-2" alt="User Image">
+          <img src="uploads/avatars/<?php echo $_SESSION['avatar']?>" class="img-circle elevation-3"   alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Username</a>
+          <a href="#" class="d-block"> <?php echo $_SESSION['fname'].' '.$_SESSION['lname']; ?></a>
         </div>
       </div>
 
- 
+
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column " data-widget="treeview" role="menu" data-accordion="false">
-            
+
         <!-- Dashboard -->
         <li class="nav-item ">
-            <a href="hod.php" class="nav-link text-light bg-orange">
+            <a href="coordinator.php" class="nav-link text-light bg-orange">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
-                
+
               </p>
             </a>
-     
+
           </li>
           <div class="dropdown-divider"></div>
         <!-- Manage users  -->
@@ -74,23 +74,18 @@
             <a href="#" class="nav-link bg-orange ">
               <i class="nav-icon fas fa-users"></i>
               <p>
-                Manage Students   
+                Manage Students
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="deptstaffs.php" class="nav-link ">
+                <a href="programme_students.php" class="nav-link ">
                   <i class="far fa-circle nav-icon"></i>
                   <p>All Students</p>
                 </a>
               </li>
-              <li class="nav-item">
-                <a href="AllStudents.php" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Allocations</p>
-                </a>
-              </li>
+
               <li class="nav-item">
                 <a href="AllStudents.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
@@ -100,31 +95,26 @@
             </ul>
           </li>
           <div class="dropdown-divider"></div>
- 
+
           <!-- Manage Supervision -->
           <li class="nav-item ">
             <a href="#" class="nav-link bg-orange ">
               <i class="nav-icon fas fa-user-tie"></i>
               <p>
-                Supervision
+                Supervisions
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="usupervisors.php" class="nav-link">
+                <a href="my_students.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Students</p>
+                  <p>My students</p>
                 </a>
               </li>
+
               <li class="nav-item">
-                <a href="hsupervisors.php" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Host-Supervisors</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="./index3.html" class="nav-link">
+                <a href="allmysupervisions.php" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
                   <p>History</p>
                 </a>
@@ -139,26 +129,26 @@
               <i class="nav-icon fas fa-file"></i>
               <p>
                 Reports
-                
+
               </p>
             </a>
-     
+
           </li>
           <div class="dropdown-divider"></div>
         <!-- profile -->
         <li class="nav-item ">
-            <a href="" class="nav-link text-light bg-orange">
+            <a href="user_profile.php?id=<?php echo $_SESSION['id']; ?>" class="nav-link text-light bg-orange">
               <i class="nav-icon fas fa-user"></i>
               <p>
                 Profile
-                
+
               </p>
             </a>
-     
+
           </li>
-        
+
         </ul>
-      
+
       </nav>
       <!-- /.sidebar-menu -->
     </div>
