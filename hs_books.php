040b5cf8a7d30b00097f09c2f5d72a25c3d73776
Baltazar Christian <77143206+tazarchriss@users.noreply.github.com>
2022-06-04 00:15:54 +0300

    Create hs_books.php

---
+++ hs_books.php
@@ -0,0 +1,179 @@
+<!-- This is the page for managing all the staffs  --><!-- This is a page for Admin Home page -->
+<?php
+
+    session_start();
+    $id=$_SESSION['id'];
+    $name=$_SESSION['fname'];
+    if ($_SESSION['role']!='6') {
+      header('Location:index.html');
+    }
+
+    else{
+      include 'config/connection.php';
+      $sql="SELECT * FROM allocation,attachment_request,user
+      WHERE allocation.host_supervisor='$id'
+      and attachment_request.att_id=allocation.attachment
+      and user.id=attachment_request.student
+    ";
+      $qry=mysqli_query($conn,$sql);
+
+
+      include 'include/header.php';
+    ?>
+
+<body class="hold-transition ">
+<div class="wrapper">
+
+
+
+<?php include 'include/hsnav.php'; ?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+
+    <!-- Start of Staff Table -->
+    <section class="content mt-3">
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
+            <div class="row">
+                <div class="col-md-12">
+        <div class="card">
+            <div class="card-header bg-dark">
+              <h3 class="card-title"></h3>
+            </div>
+            <!-- /.card-header -->
+            <div class="card-body">
+                     <?php
+
+                    if (mysqli_num_rows($qry) == 0){
+
+
+                    ?>
+                   <p class="text-dark text-center">Sorry, there are no students assigned to you!</p>
+
+                    <?php
+
+                    }
+
+                    else{
+
+                      ?>
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>SN</th>
+                  <th>Full Names</th>
+                  <th>Start Date</th>
+                  <th>End Date</th>
+                  <th>Actions</th>
+
+                </tr>
+                </thead>
+                <?php
+                    for ($i=1; $i<=mysqli_num_rows($qry); $i++){
+                      $row = mysqli_fetch_array($qry);
+
+
+
+
+                  ?>
+            <tr>
+              <td>
+                <?php echo $i; ?></td>
+              <td>
+                <?php echo $row['fname'].' '.$row['mname'].' '.$row['lname']; ?>
+              </td>
+
+              <td>
+              <?php echo $row['start_date']; ?>
+             </td>
+             <td>
+              <?php echo $row['end_date']; ?>
+             </td>
+
+              <td>
+                  <div class="text-center">
+                      <a class=" badge bg-orange p-1" href="SingleStudentlogs.php?id=<?php echo $row['id']; ?>"><i class="fa fa-user"> View Logbooks</i></a>
+                      <!-- <a class="bg-dark  p-1" href="config/DeleteEarning.php?id=<?php echo $row['id'];?>"><i class="">Decline</i></a> -->
+                    </div>
+              </td>
+            </tr>
+            <?php
+              }
+            }
+              ?>
+              </tbody>
+              </table>
+            </div>
+            <!-- /.card-body -->
+          </div>
+        </div>
+          </div>
+          </div>
+    </section>
+
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer bg-dark text-center">
+    <br>
+    <strong>Copyright &copy; 2021/2022 <a href="" class="text-orange">TazarChriss </a>.</strong>
+    All rights reserved.
+    <div class="mt-4">
+        <h1></h1>
+    </div>
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables  & Plugins -->
+<script src="plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="plugins/jszip/jszip.min.js"></script>
+<script src="plugins/pdfmake/pdfmake.min.js"></script>
+<script src="plugins/pdfmake/vfs_fonts.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+<!-- Page specific script -->
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["pdf"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
+</body>
+</html>
+
+
+<?php
+
+}
+?>
